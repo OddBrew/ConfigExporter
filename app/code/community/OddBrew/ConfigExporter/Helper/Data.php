d7fab2b25962ece9986c2ecae18f59cfdb77b802
Dakstraede <alexandre.fayette@gmail.com>
2016-08-18 22:30:11 +0200

    add menu item and base helper

---
+++ app/code/community/OddBrew/ConfigExporter/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    OddBrew
+ * @package     OddBrew_ConfigExporter
+ * @author      Alexandre Fayette <alexandre.fayette@gmail.com>
+ * @link        https://github.com/OddBrew/ConfigExporter
+ * @copyright   Copyright (c) 2016 Alexandre Fayette
+ * @license     https://opensource.org/licenses/MIT  MIT License
+ */
+class OddBrew_ConfigExporter_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}

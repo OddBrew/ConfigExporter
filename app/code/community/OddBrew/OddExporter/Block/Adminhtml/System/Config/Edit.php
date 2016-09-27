bbc7cca65e5b0516d32f1f4d0b240014a27a9968
OddBrew <alexandre.fayette@gmail.com>
2016-09-27 15:52:04 +0200

    add export section button to config page

---
+++ app/code/community/OddBrew/OddExporter/Block/Adminhtml/System/Config/Edit.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @category    OddBrew
+ * @package     OddBrew_Exporter
+ * @author      Alexandre Fayette <alexandre.fayette@gmail.com>
+ * @link        https://github.com/OddBrew/ConfigExporter
+ * @copyright   Copyright (c) 2016 Alexandre Fayette
+ * @license     https://opensource.org/licenses/MIT  MIT License
+ */
+class OddBrew_OddExporter_Block_Adminhtml_System_Config_Edit extends Mage_Adminhtml_Block_System_Config_Edit
+{
+
+    protected function _prepareLayout()
+    {
+        $section = Mage::app()->getRequest()->getParam('section');
+        $url = Mage::helper('adminhtml')->getUrl('*/oddexporter_config_export/section', array(
+            'section' => $section,
+            'store' => Mage::app()->getRequest()->getParam('store'),
+            'website' => Mage::app()->getRequest()->getParam('website')
+        ));
+
+        $this->setChild('export_config_section',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->setData(array(
+                    'label'     => $this->__('Export Section'),
+                    'onclick'   => "setLocation('{$url}'); return false;",
+                    'class' => 'action',
+                ))
+        );
+
+        return parent::_prepareLayout();
+    }
+
+    public function getSaveButtonHtml()
+    {
+        $html = $this->getChildHtml('save_button');
+        $html .= $this->getExportSectionButtonHtml();
+
+        return $html;
+    }
+
+    public function getExportSectionButtonHtml()
+    {
+        return $this->getChildHtml('export_config_section');
+    }
+}
